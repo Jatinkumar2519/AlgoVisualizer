92c25bc1510eb1006746976462ba80c7b9c4c24a
Jatinkumar2519 <149255592+Jatinkumar2519@users.noreply.github.com>
2025-08-07 09:21:32 +0530

    installed react-konva

--- src/Components/Visualizers.js
+++ src/Components/Visualizers.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import {Stage,Layer,Rect} from 'react-konva'
 export default function Visualizers() {
+  const arr = [10,20,30,20,5];
   return (
-    <div>
-      here is visualizations of the algos
-    </div>
+    <Stage>
+      <Layer>
+        {arr.map((height,index) => {
+          <Rect
+          key={index}
+          x={index * 60 + 10}
+          y={300 - height}
+          width={40}
+          height={height}
+          fill="skyblue"
+          stroke="black"
+          />
+        })}
+      </Layer>
+    </Stage>
   )
 }
